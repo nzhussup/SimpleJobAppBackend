157d799e1d0ed3e1b504f82ebf6153f14e916ef9
nzhussup <nurzhanat2002@gmail.com>
2024-09-30 23:10:30 +0200

    Refactor Job model / Add lombok dependency

--- src/main/java/com/nzhussup/jobappproject/model/Job.java
+++ src/main/java/com/nzhussup/jobappproject/model/Job.java
@@ -1,9 +1,13 @@
 package com.nzhussup.jobappproject.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Component
 public class Job {
 
@@ -13,59 +17,9 @@ public class Job {
     private String city;
     private String requirements;
 
-    public Job() {
-    }
 
     public Job(String name, String country, String city, String requirements) {
         this(0, name, country, city, requirements);
-    }
-
-    public Job(int id, String name, String country, String city, String requirements) {
-        this.id = id;
-        this.name = name;
-        this.country = country;
-        this.city = city;
-        this.requirements = requirements;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getRequirements() {
-        return requirements;
-    }
-
-    public void setRequirements(String requirements) {
-        this.requirements = requirements;
     }
 
     @Override
